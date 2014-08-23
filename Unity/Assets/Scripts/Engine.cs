d44a35d54d55b3d04a46f13481baae8f3f64f5ad
Daniel Epstein <depstein@cs.washington.edu>
2014-08-23 09:33:39 -0700

    Code reorganization, Ben's gonna kill me

--- Unity/Assets/Scripts/Engine.cs
+++ Unity/Assets/Scripts/Engine.cs
@@ -5,27 +5,6 @@ using System.Text;
 
 namespace Scripts
 {
-    public static class EnumerableAdditions
-    {
-        public static IEnumerable<T> RandomItems<T>(this IEnumerable<T> items, Random random, int count)
-        {
-            return ShuffleIterator(items, random).Take(count);
-        }
-
-        public static IEnumerable<T> ShuffleIterator<T>(
-            this IEnumerable<T> source, Random rng)
-        {
-            var buffer = source.ToList();
-
-            for (int i = 0; i < buffer.Count; i++)
-            {
-                int j = rng.Next(i, buffer.Count);
-                yield return buffer[j];
-
-                buffer[j] = buffer[i];
-            }
-        }
-    }
     public class Engine
     {
         public Engine(List<Person> people)
---
+++ Unity/Assets/Scripts/Utility/EnumerableAdditions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scripts
+{
+    public static class EnumerableAdditions
+    {
+        public static IEnumerable<T> RandomItems<T>(this IEnumerable<T> items, Random random, int count)
+        {
+            return ShuffleIterator(items, random).Take(count);
+        }
+
+        public static IEnumerable<T> ShuffleIterator<T>(
+            this IEnumerable<T> source, Random rng)
+        {
+            var buffer = source.ToList();
+
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                int j = rng.Next(i, buffer.Count);
+                yield return buffer[j];
+
+                buffer[j] = buffer[i];
+            }
+        }
+    }
+}
