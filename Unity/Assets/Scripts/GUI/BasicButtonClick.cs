120f89ff8c36c61ed5806ef30e621df01f7c370d
Nick Loffredo <nloffred@gmail.com>
2014-08-22 23:21:06 -0700

    Dynamic button text, y'all!

--- Unity/Assets/Scripts/GUI/BasicButtonClick.cs
+++ Unity/Assets/Scripts/GUI/BasicButtonClick.cs
@@ -2,8 +2,12 @@
 
 public class BasicButtonClick : MonoBehaviour {
 
+	int count=0;
 	void OnClick ()
 	{
 		print ("CLICKING");
+		UILabel c = GameObject.Find ("Label (btn Text)").GetComponent<UILabel> ();
+		c.text = "You have killed " + count + " grues";
+		count++;
 	}
 }
