0b8e9afd1db17609878e3a9c0aaa85866891013f
Ben Witman <benwitman@benwitman.com>
2014-08-23 12:38:13 -0700

    Update PersonUI to support speech bubbles - THEY CAN TALK!

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -21,16 +21,16 @@ namespace Scripts
         void ShowPeopleSelection()
         {
             Options.ForEach(t => t.Hide());
-            People.ForEach(t => t.HideOptionUI());
+            People.ForEach(t => t.Hide());
             var selection = engine.NextPossiblePeople();
-            selection.People.ForEach((t, i) => People[i].DisplayOptionUI(t, () => PersonSelected(People[i], selection, t)));
+            selection.People.ForEach((t, i) => People[i].ShowThoughtBubble(t, () => PersonSelected(People[i], selection, t)));
         }
 
         void PersonSelected(GUI.PersonUI person, PersonSelection selection, PersonSelectionOption personSelected)
         {
             var dialogEngine = engine.AcceptDialogEntry(selection, personSelected);
 
-            People.Where(t => t != person).ForEach(t => t.HideOptionUI());
+            People.Where(t => t != person).ForEach(t => t.Hide());
 
             RunDialog(dialogEngine, person);
         }
