6bbf417a146232eb817471248161243a91b96c08
Daniel Epstein <depstein@cs.washington.edu>
2014-08-23 01:39:10 -0700

    Huzzah, we have colors. I'm tired. Zzzzzzzzz

--- Python/colors.py
+++ Python/colors.py
@@ -7,7 +7,5 @@ from pprint import pprint
 
 colors = wikipedia.page("List_of_colors_(compact)")
 html = colors.html()
-print "Dictionary<string,string> colors = new Dictionary<string,string>() {"
 for color in re.findall('<p title=.*?#(.*?)\" style=\"[\s\S]*?\" title=\".+\">(.+?)</a>', html):
-	print "{\"%s\",\"#%s\"}," % (re.sub('[\W_]+', '', color[1]).upper(), color[0])
-print "}"
+	print "public const string %s = \"#%s\";" % (re.sub('[\W_]+', '', color[1].title()), color[0])
