6c892f455a976da36e5ed2986f3603abdf07c173
Daniel Epstein <depstein@cs.washington.edu>
2014-08-23 16:29:47 -0700

    thank goodness the colors didn't disappear.

--- Python/colors.py
+++ Python/colors.py
@@ -5,7 +5,16 @@ import re
 import string
 from pprint import pprint
 
-colors = wikipedia.page("List_of_colors_(compact)")
-html = colors.html()
+colors_page = wikipedia.page("List_of_colors_(compact)")
+html = colors_page.html()
+colors = {}
 for color in re.findall('<p title=.*?#(.*?)\" style=\"[\s\S]*?\" title=\".+\">(.+?)</a>', html):
-	print "public const string %s = \"#%s\";" % (re.sub('[\W_]+', '', color[1].title()), color[0])
+	name = re.sub('[\W_]+', '', color[1].title())
+	if name in colors:
+		name = name + '2'
+	colors[name] = "\tpublic const string %s = \"#%s\";" % (name, color[0])
+
+print "public class Colors {"
+for key,value in sorted(colors.items()):
+	print value
+print "}"
