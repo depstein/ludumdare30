b20e42aed45cfb18d52110e652311896f966a4c6
Ben Witman <benwitman@benwitman.com>
2014-08-23 16:29:35 -0700

    adding initial planets

---
+++ Unity/Assets/Scripts/Planet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    public float InitialXVelocity;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(InitialXVelocity, 0.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
