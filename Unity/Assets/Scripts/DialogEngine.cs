c42381ca05fdd9c3c920e233730ffabbd5e30556
Ben Witman <benwitman@benwitman.com>
2014-08-22 22:10:16 -0700

    oops, scripts need to be in assets

---
+++ Unity/Assets/Scripts/DialogEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scripts
+{
+    public class DialogEngine
+    {
+
+        public DialogEngine(DialogTree tree, Context context)
+        {
+            this.dialogTree = tree;
+            this.context = context;
+            this.CurrentNode = tree.RootNode;
+        }
+
+        public void TakeOption(string option)
+        {
+            this.context.PromptUsed(dialogTree, CurrentNode, option);
+            this.CurrentNode = this.CurrentNode.Answers[option];
+        }
+
+        private Context context;
+        private DialogTree dialogTree;
+        public DialogNode CurrentNode { get; private set; }
+    }
+}
