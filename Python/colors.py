756be8326d3ca283bdb4d9067f56b8dd95c7d326
Daniel Epstein <depstein@cs.washington.edu>
2014-08-23 18:13:57 -0700

    Disco background!

--- Python/colors.py
+++ Python/colors.py
@@ -3,18 +3,20 @@
 import wikipedia
 import re
 import string
+import struct
 from pprint import pprint
 
 colors_page = wikipedia.page("List_of_colors_(compact)")
 html = colors_page.html()
 colors = {}
-for color in re.findall('<p title=.*?#(.*?)\" style=\"[\s\S]*?\" title=\".+\">(.+?)</a>', html):
+for color in re.findall('<p title=.*?#+(.*?)\" style=\"[\s\S]*?\" title=\".+\">(.+?)</a>', html):
 	name = re.sub('[\W_]+', '', color[1].title())
 	if name in colors:
 		name = name + '2'
-	colors[name] = "\tpublic const string %s = \"#%s\";" % (name, color[0])
+	r,g,b = struct.unpack('BBB',color[0].decode('hex'))
+	colors[name] = "\tpublic static readonly Color %s = new Color(%0.3ff, %0.3ff, %0.3ff, 1f);" % (name, r/255.0, g/255.0, b/255.0)
 
-print "public class Colors {"
+print "public static class Colors {"
 for key,value in sorted(colors.items()):
 	print value
 print "}"
