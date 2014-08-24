54633ca0afc868ac5db19ced034a7e9d4765cc92
Daniel Epstein <depstein@cs.washington.edu>
2014-08-23 21:23:33 -0700

    Changed the scene.

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -6,7 +6,7 @@ public class Main : MonoBehaviour {
 	
 	public GameObject gravityObject;
 	public static GameObject staticGravityObject;
-	public static float GravitationalConstant = .025f;
+	public static float GravitationalConstant = .1f;
 	void Awake() {
 		staticGravityObject = gravityObject;
 	}
@@ -14,7 +14,7 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//InvokeRepeating("SpawnPlanet", 2, 1f);
-		for (int x = 0; x < 5; x++)
+		for (int x = 0; x < 10; x++)
 		{
 			SpawnPlanet();
 		}
