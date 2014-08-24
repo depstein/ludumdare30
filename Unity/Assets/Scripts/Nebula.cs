5b235d06cdba6813f2f4de116a569a9295a03df9
Daniel Epstein <depstein@cs.washington.edu>
2014-08-24 13:33:32 -0700

    More nebulae changes. I'm addicted, I should stop.

--- Unity/Assets/Scripts/Nebula.cs
+++ Unity/Assets/Scripts/Nebula.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 using System.Collections;
 
 public class Nebula : MonoBehaviour {
-	private Color color1 = Colors.White;
-	private Color color2 = Colors.White;
-	private Color[] destinations = {Colors.GreenYellow, Colors.NeonGreen, Colors.BrightPink, Colors.Lemon, 
-		Colors.SpiroDiscoBall, Colors.Ruddy, Colors.PurplePizzazz, Colors.Celeste, Colors.White};
+	private Color color1 = new Color(1f, 1f, 1f, 0.5f);
+	private Color color2 = new Color(1f, 1f, 1f, 0.5f);
 	private bool oneTwo = false;
 	private float colorFrequency;
 	private float rotationMultiplier;
 	private float alpha;
 	private Vector3 movement;
+	private float prevTime;
 
 	// Use this for initialization
 	void Start () {
 		colorFrequency = Random.Range(2f, 8f);
 		rotationMultiplier = Random.Range(5f, 10f) * (Random.value >= 0.5 ? 1 : -1);
 		alpha = Random.Range(.1f, .5f);
-		Vector2 move = Random.insideUnitCircle * Random.Range(0.001f, 0.1f) * (Random.value >= 0.5 ? 1 : -1);
+		Vector2 move = Random.insideUnitCircle * Random.Range(0.001f, 0.05f) * (Random.value >= 0.5 ? 1 : -1);
 		movement = new Vector3(move.x, move.y, 0);
-		InvokeRepeating("ChangeColor", colorFrequency, colorFrequency);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float timeModded = (Time.time % colorFrequency)/colorFrequency;
-		Color next = Color.Lerp(color1, color2, oneTwo?timeModded:1-timeModded);
-		next.a = alpha;
-		this.GetComponent<SpriteRenderer>().color = next;
+		if(timeModded<prevTime) {
+			ChangeColor();
+		}
+
+		this.GetComponent<SpriteRenderer>().color = Color.Lerp(color1, color2, oneTwo?timeModded:1-timeModded);
 		transform.Rotate(Vector3.forward * Time.deltaTime * rotationMultiplier);
 		transform.position += movement;
 		if(Mathf.Abs(transform.position.x)>=40) {
@@ -36,22 +36,21 @@ public class Nebula : MonoBehaviour {
 		if(Mathf.Abs(transform.position.y)>=25) {
 			movement.y *= -1;
 		}
+		prevTime = timeModded;
 	}
 
 	void ChangeColor() {
 		oneTwo = !oneTwo;
 		if(oneTwo) {
-			color2 = NextColor(color1);
+			color2 = NextColor();
 		} else {
-			color1 = NextColor(color2);
+			color1 = NextColor();
 		}
 	}
 
-	Color NextColor(Color current) {
-		Color next = destinations[Utility.random.Next(destinations.Length)];
-		while(next == current) {
-			next = destinations[Utility.random.Next(destinations.Length)];
-		}
-		return next;
+	Color NextColor() {
+		float h, s, v;
+		Colors.ColorToHSV(Colors.GetRandomColor(), out h, out s, out v);
+		return Colors.ColorFromHSV(h, 0.3f, v, alpha);
 	}
 }
