1dd75f03ec6eff64ba7d74f36f270afd0137b2ca
Nick Loffredo <nloffred@gmail.com>
2014-08-23 21:00:58 -0700

    ORBITS

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -6,11 +6,21 @@ public class Main : MonoBehaviour {
 	public GameObject background;
 	private Color sourceColor = Colors.White;
 	private Color destinationColor;
+	public GameObject gravityObject;
+	public static GameObject staticGravityObject;
+	public static float GravitationalConstant = 10;
+	void Awake() {
+		staticGravityObject = gravityObject;
+	}
 
 	// Use this for initialization
 	void Start () {
 		destinationColor = Colors.GetRandomColor();
-		InvokeRepeating("SpawnPlanet", 2, 1f);
+		//InvokeRepeating("SpawnPlanet", 2, 1f);
+		for (int x = 0; x < 8; x++)
+		{
+			SpawnPlanet();
+		}
 		InvokeRepeating("ChangeColor", 1, 1f);
 	}
 	
@@ -25,7 +35,13 @@ public class Main : MonoBehaviour {
 	}
 
 	void SpawnPlanet() {
-		GameObject ret = Instantiate(planetPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-		ret.GetComponent<Rigidbody2D>().velocity = 5 * Random.insideUnitCircle;
+		GameObject ret = Instantiate(planetPrefab, Random.insideUnitCircle * 10, Quaternion.identity) as GameObject;
+
+		float angleBetweenVelocityAndRadius = Mathf.PI / 2;
+		Vector3 radius = (Main.staticGravityObject.transform.position - ret.transform.position);
+		float vel = Mathf.Sqrt (GravitationalConstant * staticGravityObject.rigidbody2D.mass / radius.magnitude);
+		
+		Vector3 dir = Vector3.Cross (radius.normalized, new Vector3 (0, 0, 1));
+		ret.GetComponent<Rigidbody2D> ().velocity = vel * dir;//5 * Random.insideUnitCircle;
     }
 }
