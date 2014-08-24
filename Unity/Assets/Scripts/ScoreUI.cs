5704b72e4c61df54326dd19aa576adb96814631b
Nick Loffredo <nloffred@gmail.com>
2014-08-24 14:43:15 -0700

    Gameplay hint

--- Unity/Assets/Scripts/ScoreUI.cs
+++ Unity/Assets/Scripts/ScoreUI.cs
@@ -5,9 +5,11 @@ public class ScoreUI : MonoBehaviour {
 
     public UILabel ScoreLabel;
 	public UILabel HighScoreLabel;
+	public UILabel gameplayHint;
 
     void Awake() {
 		StaticScoreboard.ScoreLabel = this.ScoreLabel;
 		StaticScoreboard.HighScoreLabel = this.HighScoreLabel;
+		GameObject.Destroy (gameplayHint, 5f);
 	}
 }
